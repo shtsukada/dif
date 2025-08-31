739e8189a26f2ef48981d710768011fe65a1ea4d
shtsukada <st01251988@gmail.com>
2025-08-31 15:55:57 +0900

    goget

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/storage"
+	"fyne.io/fyne/v2/widget"
+
+	"github.com/pmezard/go-difflib/difflib"
+)
+
+func main() {
+
+}
